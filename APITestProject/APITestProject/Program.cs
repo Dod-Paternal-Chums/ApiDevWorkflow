30089cff02dd7109c43ab3af79c6c0a9322dbe51
Matthew Miller <matthew.w.miller147@outlook.com>
2023-02-28 08:44:41 -0800

    Packages added for Discord, Added first part of a bungie call. needs API key.

--- APITestProject/APITestProject/Program.cs
+++ APITestProject/APITestProject/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using APITestProject.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
 
 namespace APITestProject
 {
     class Program
     {
+        public static string MainBungie { get; set; }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            MainBungie = "https://www.bungie.net/platform/Destiny/";
+            var item = getBungieUser();
+        }
+
+        static Response<bungieNetUser> getBungieUser()
+        {
+            var location = new Uri("");
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-API-Key", "YOUR-API-KEY-HERE");
+                string jsonResponse = client.GetStringAsync(MainBungie + location).Result;
+                return JsonConvert.DeserializeObject<Response<bungieNetUser>>(jsonResponse);
+            }
         }
     }
 }
