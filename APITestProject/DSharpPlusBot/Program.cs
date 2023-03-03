f9b144c1120bb4661ec609df72d27e6bb2f7b696
Matthew Miller <matthew.w.miller147@outlook.com>
2023-03-03 13:16:37 -0800

    Added DSharpPlus bot with very basic command.

---
+++ APITestProject/DSharpPlusBot/Program.cs
@@ -0,0 +1,30 @@
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+
+namespace DSharpPlusBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "MTA3OTg0NjQ4OTQzNTY4NTAwOQ.Gjrzl7.fBeziEa-IzmuUIKcuZiYUeSe9U98a-cacsGH4U",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("omega"))
+                    await e.Message.RespondAsync("is a bitch");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
